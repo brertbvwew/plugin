535f5ff3e0e8125e27b8999657f020ad35a40f54
edfbedbnedbn <97176419+brertbvwew@users.noreply.github.com>
2026-01-28 11:44:41 +0200

    Neo Elite Update: tools-qc.js

---
+++ plugin/tools-qc.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text }) => {
+  m.reply('Hello World!')
+}
+
+handler.command = ['tools-qc']
+handler.group = false   
+handler.admin = false   
+handler.owner = false   
+handler.premium = false 
+handler.private = false 
+
+export default handler
