9a72ca3f2d0aa5afd30fcdcaa755d011f9892dd7
edfbedbnedbn <97176419+brertbvwew@users.noreply.github.com>
2026-01-28 12:13:00 +0200

    Neo Elite Update: tools-id.js

--- plugin/tools-id.js
+++ plugin/tools-id.js
@@ -1,11 +1,18 @@
-let handler = async (m, { conn, text }) => {
-  m.reply('Hello World!')
+
+let handler = async (m, { sock, text, prefix, command, reply }) => {
+    const sender = m.key.participant || m.key.remoteJid
+    const userId = sender.split('@')[0]
+    await reply(`*Your ID:* ${userId}\n\nCopy this number and put it in your config.js owner array.`)
 }
 
-handler.command = ['tools-id']
+handler.command = /^(getid)$/i 
+handler.tags = ['tools']
+handler.help = ['getid']
+
+// 5. Requirements (True/False)
 handler.group = false   
 handler.admin = false   
-handler.owner = false   
+handler.owner = false 
 handler.premium = false 
 handler.private = false 
 
